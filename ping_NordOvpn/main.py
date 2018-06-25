f335d4be4e81d02ff6a8514bce0021e5564937cf
name <you@example.com>
2018-06-25 17:26:04 +0800

    Add Ignore list @ ping_NordOvpn

--- ping_NordOvpn/main.py
+++ ping_NordOvpn/main.py
@@ -4,6 +4,7 @@ import queue
 from threading import Thread, Lock
 from cus_ping import *
 from zipfile import ZipFile
+from struct import pack, unpack
 
 OVPN_ZIP_PATH = 'D:\\Download'
 OVPN_TMP_FORDER = OVPN_ZIP_PATH + '\\.nord.tmp'
@@ -84,14 +85,16 @@ class ParseOvpn(Thread):
 
 
 class PingServer(Thread):
-    def __init__(self,q_server, delay_list):
+    def __init__(self,q_server, delay_list, ignore_set=set()):
         assert(isinstance(q_server, queue.Queue))
         assert(isinstance(delay_list, list))
+        assert(isinstance(ignore_set, set))
         Thread.__init__(self)
         self.data_lock = Lock()
         self.processed_num = 0
         self.q_server = q_server
         self.delay_list = delay_list
+        self.ignore_set = ignore_set
         try:
             self.total_num = self.q_server.get(True, timeout=0.3)
         except queue.Empty:
@@ -116,7 +119,10 @@ class PingServer(Thread):
             try:
                 s_info = self.q_server.get(True, timeout=0.1)
                 assert(isinstance(s_info, ServerInfo))
-                s_info.delay = cus_ping(s_info.ip)
+                if s_info.ip in self.ignore_set:
+                    s_info.delay = 8888
+                else:
+                    s_info.delay = cus_ping(s_info.ip)
                 num = self.inc_processed_num()
                 print('{}/{}: {}'.format(num, self.total_num, str(s_info)))
                 self.delay_list.append((s_info.delay, s_info.name, s_info.ip, s_info.path))
@@ -136,15 +142,44 @@ class PingServer(Thread):
             th.join()
 
 
+class IgnoreSet:
+    OVPN_IGNORE_FILE = OVPN_ZIP_PATH + '\\.OVPN_Ignore.list'
+
+    @staticmethod
+    def get():
+        ip_set = set()
+        if os.path.exists(IgnoreSet.OVPN_IGNORE_FILE):
+            size = os.path.getsize(IgnoreSet.OVPN_IGNORE_FILE)
+            f = open(IgnoreSet.OVPN_IGNORE_FILE, 'rb')
+            for i in range(size//4):
+                ip = f.read(4)
+                ip_array = unpack('BBBB', ip)
+                ip_set.add('.'.join([str(n) for n in ip_array]))
+            f.close()
+        return ip_set
+
+    @staticmethod
+    def set(delay_list):
+        assert(isinstance(delay_list, list))
+        f = open(IgnoreSet.OVPN_IGNORE_FILE, 'wb')
+        for delay, name, ip, path in delay_list:
+            assert(isinstance(ip, str))
+            if delay > 2000:
+                ip = [int(n) for n in ip.split('.')]
+                f.write(pack('BBBB', ip[0], ip[1], ip[2], ip[3]))
+        f.close()
+
+
 if __name__ == '__main__':
     delay_list = []
     q_file = queue.Queue()
     q_server_info = queue.Queue()
     Thread(target=unzip_ovpn_zip, args=(OVPN_ZIP_PATH, OVPN_TMP_FORDER, q_file)).start()
     ParseOvpn(q_file, q_server_info)
-    th = PingServer(q_server_info, delay_list)
+    th = PingServer(q_server_info, delay_list, IgnoreSet().get())
     th.join()
     print('================================')
     delay_list.sort()
+    IgnoreSet.set(delay_list)
     for x in delay_list[0:10]:
         print(x)
